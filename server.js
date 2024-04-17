85bc5eb5d9f5a94213cc986925f43bac29cf777f
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-04-17 16:22:23 +0300

    update proxy server

--- server.js
+++ server.js
@@ -1,50 +1,45 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const dotenv = require('dotenv');
-
+const express = require("express");
+const server = require("http").createServer(app);
+const dotenv = require("dotenv");
+const httpProxy = require("http-proxy");
 dotenv.config();
 
+// Create Express Server
 const app = express();
+
+// Environment Variables
 const PORT = process.env.PORT || 3000;
 const API_SERVICE_URL = process.env.API_SERVICE_URL;
 
-// Middleware for checking the Authorization header
-function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-    console.log(authHeader);
-    if (authHeader) {
-        const token = authHeader.split(' ')[1]; // Assumes the format is "Bearer token"
-        // Verify the token against the expected API key
-        if (token === process.env.API_KEY) {
-            next();
-        } else {
-            res.status(403).json({ error: 'Unauthorized access: Invalid token' });
-        }
-    } else {
-        res.status(401).json({ error: 'Unauthorized access: No credentials sent' });
-    }
-}
-
-// Proxy endpoint configuration
-const proxyOptions = {
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    ws: true,
-    pathRewrite: {
-        [`^/api`]: '',
-    },
-    onProxyReq: (proxyReq, req, res) => {
-        // Set a new authorization header for the outgoing request to the API
-        proxyReq.setHeader('Authorization', `Bearer ${process.env.NEW_API_AUTH_TOKEN}`);
-    }
+// Proxy Server Options
+const options = {
+ target: API_SERVICE_URL,
+ changeOrigin: true,
+ ws: true,
+ pathRewrite: {
+    [`^/graphql`]: "",
+ },
+ onProxyReq: (proxyReq, req, res) => {
+    // Set a new authorization header for the outgoing request to the API
+    proxyReq.setHeader("Authorization", `Bearer ${process.env.INKEEP_API_KEY}`);
+ },
 };
 
-// Use the authentication middleware
-app.use(authMiddleware);
+const proxy = httpProxy.createProxyServer(options);
 
-// Use the configured proxy middleware
-app.use('/api', createProxyMiddleware(proxyOptions));
+// Handle all requests
+app.all("/*", (req, res) => {
+ console.log("Request ALL");
+ proxy.web(req, res);
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Handle WebSocket upgrade requests
+server.on("upgrade", (req, socket, head) => {
+ console.log("connecting");
+ proxy.ws(req, socket, head);
+});
+
+// Start the server
+server.listen(PORT, () => {
+ console.log(`Starting proxy server at:${PORT}`);
 });
